edf40906d45ab2f65bce9c232c54086573def303
Tom Bracey <tombracey22@gmail.com>
2025-08-03 21:17:24 +0100

    added gemini usage tracking

--- src/GCP_utils/gemini.py
+++ src/GCP_utils/gemini.py
@@ -1,6 +1,26 @@
 import os
 from dotenv import load_dotenv
+import json
+from datetime import date
 import google.generativeai as genai
+
+def log_gemini_usage():
+    """Logs each Gemini API call into the JSON file, grouped by day"""
+    
+    gemini_usage = "./data/gemini_usage.json"
+
+    with open(gemini_usage, "r") as f:
+        try:
+            usage_data = json.load(f)
+        except:
+            usage_data = {}
+
+    today = str(date.today())
+    usage_data[today] = usage_data.get(today, 0) + 1
+
+    with open(gemini_usage, "w") as f:
+        json.dump(usage_data, f, indent=2)
+
 
 def gemini(prompt):
     load_dotenv()
@@ -9,6 +29,8 @@ def gemini(prompt):
 
     if not api_key:
         raise ValueError("API key not set.")
+    
+    log_gemini_usage()
 
     model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content(prompt)
