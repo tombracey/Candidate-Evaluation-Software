bb9792dc62471dc15a16838de2287ec3d1c33f93
Tom Bracey <tombracey22@gmail.com>
2025-08-10 23:44:28 +0100

    data updates

--- src/create_mock_data/spreadsheet.py
+++ src/create_mock_data/spreadsheet.py
@@ -16,14 +16,14 @@ def get_postcodes(n, filepath="data/postcode_sample.csv"):
 def generate_candidates(num):
     candidates_names = [fake.name() for _ in range(num)]
     candidates_postcodes = get_postcodes(num)
-    experience = [random.randint(2, 10) for _ in range(num)]
-    qualifications = [random.randint(2, 10) for _ in range(num)]
+    experience = [random.randint(20, 100) for _ in range(num)]
+    qualifications = [random.randint(20, 100) for _ in range(num)]
 
     candidates = zip(candidates_names, candidates_postcodes, experience, qualifications)
 
     with open('data/mock_candidates.csv', 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['Name', 'Postcode', 'Experience/10', 'Qualifications/10'])
+        writer.writerow(['Name', 'Postcode', 'Experience/100', 'Qualifications/100'])
         writer.writerows(candidates)
 
 def generate_employer():
